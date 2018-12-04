1b4f039ce4135412d38668832ac3bc2295436c5b
ahammaha <mahalakshmi@outlook.com>
2018-12-04 10:12:06 +0530

    images toolbar config correction

--- src/MailEditor.js
+++ src/MailEditor.js
@@ -9,14 +9,40 @@ class MailEditor extends React.Component{
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	      editorState: EditorState.createEmpty()
+	      editorState: EditorState.createEmpty(),
+	      uploadedImages: []
 	    };
 	    this.onEditorStateChange=this.onEditorStateChange.bind(this);
+	    this.uploadImageCallBack=this.uploadImageCallBack.bind(this);
 	}
 	onEditorStateChange=(editorState) => {
 		this.setState({editorState});
 		this.props.updateMailContent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
-	};
+	}
+	uploadImageCallBack(file){
+		// long story short, every time we upload an image, we
+	    // need to save it to the state so we can get it's data
+	    // later when we decide what to do with it.
+	    // Make sure you have a uploadedImages: [] as your default state
+    	let uploadedImages = this.state.uploadedImages;
+    	const imageObject = {
+	      file: file,
+	      localSrc: URL.createObjectURL(file),
+	    }
+	    uploadedImages.push(imageObject);
+
+	    this.setState({uploadedImages: uploadedImages})
+	    
+	    // We need to return a promise with the image src
+	    // the img src we will use here will be what's needed
+	    // to preview it in the browser. This will be different than what
+	    // we will see in the index.md file we generate.
+	    return new Promise(
+	      (resolve, reject) => {
+	        resolve({ data: { link: imageObject.localSrc } });
+	      }
+	    )
+	}
 	render(){
 		return(
 			<Editor
@@ -31,7 +57,14 @@ class MailEditor extends React.Component{
 					options: ['inline', 'fontSize', 'fontFamily',
 								 'list', 'textAlign', 'colorPicker',
 								 'link', 'image'],
-					textAlign: { inDropdown: true },
+					textAlign: { 
+						inDropdown: true,
+						options: ['left', 'center', 'right'],
+					},
+					fontFamily: {
+						options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
+						dropdownClassName:'email-editor-font-family'
+					},
 					inline:{
 						options: ['bold', 'italic', 'underline'],
 					},
@@ -40,6 +73,20 @@ class MailEditor extends React.Component{
 					},
 					fontSize:{
 						options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
+					},
+					link:{
+						defaultTargetOption:'_blank',
+						popupClassName:"mail-editor-link"
+					},
+					image:{
+						urlEnabled: true,
+						uploadEnabled:true,
+						uploadCallback:this.uploadImageCallBack,
+						alignmentEnabled: true,
+						defaultSize: {
+							height: 'auto',
+							width: 'auto',
+					    }
 					}
 				}
   				}
